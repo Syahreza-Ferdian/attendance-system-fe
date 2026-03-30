39c6eab07e36b690c708367f44cfaad2b95b09b6
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2026-03-30 07:53:11 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind is working!</h1>
+    </div>
+  );
+}
+export default App;
