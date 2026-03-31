616e4b2862045a88ea71b6f6bd1af4e8a593bd53
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2026-03-31 15:12:58 +0700

    `feat`: update App component and main entry point for improved layout and routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black">
-      <h1 className="text-4xl font-bold text-blue-600">Tailwind is working!</h1>
+    <div className="bg-surface text-ink dark:bg-surface-dark dark:text-ink-inverse shadow-card rounded-2xl p-4">
+      Hello World
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/index.tsx";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+
+    <Toaster
+      position="top-right"
+      toastOptions={{
+        duration: 5000,
+        style: {
+          fontFamily: "'Plus Jakarta Sans', sans-serif",
+          fontSize: "18px",
+          borderRadius: "12px",
+          padding: "12px 16px",
+          boxShadow: "0 4px 12px rgb(0 0 0 / 0.1)",
+        },
+      }}
+    />
   </StrictMode>,
-)
+);
